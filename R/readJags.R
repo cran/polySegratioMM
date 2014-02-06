73fae4027e49008c3e0264ac8ebc4c222533c8d5
Peter Baker <p.baker1@uq.edu.au>
2014-02-06 00:00:00 +0000

    version 0.6-3

--- R/readJags.R
+++ R/readJags.R
@@ -4,7 +4,7 @@ function(run.jags, quiet=TRUE, ...)
   ## wrapper to read.openbugs so ... can be used for start, end, thin
   ## returns object of class mcmc.list
   
-  require(coda)
+  ## require(coda) # obsolete in recent versions of R as in Depends
 
   if (class(run.jags) != "runJags")
     stop("'run.jags' must be of class 'runJags'")
@@ -15,7 +15,7 @@ function(run.jags, quiet=TRUE, ...)
   ##                      quiet=quiet, ...)
   ##  x.jags <- mcmc.list(x.jags)
   ##} else {
-  x.jags <- read.openbugs(stem = run.jags$jags.control$stem, quiet=quiet, ...)
+  x.jags <- coda::read.openbugs(stem = run.jags$jags.control$stem, quiet=quiet, ...)
   ##}
   
   res <- list(run.jags=run.jags, mcmc.list=x.jags)
